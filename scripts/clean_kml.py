3634447cfcb507327377917ab48cb21738b45412
salinawu <salinawu@uchicago.edu>
2015-11-22 11:34:55 -0600

    pink gangs

--- scripts/clean_kml.py
+++ scripts/clean_kml.py
@@ -3,7 +3,7 @@ new = open('../new_chicago_gang_map.kml', 'w')
 with open('../chicago_gang_map.kml', 'r') as f:
   for line in f:
      if 'color' in line:
-         new.write("<color>f0ff0000</color>\n")
+         new.write("<color>50A078F0</color>\n")
      else:
          new.write(line)
 
