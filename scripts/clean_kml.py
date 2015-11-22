927491a7105fc63a28690b897c7766ff37b21de1
salinawu <salinawu@uchicago.edu>
2015-11-21 21:18:39 -0600

    fix transparency

--- scripts/clean_kml.py
+++ scripts/clean_kml.py
@@ -3,7 +3,7 @@ new = open('../new_chicago_gang_map.kml', 'w')
 with open('../chicago_gang_map.kml', 'r') as f:
   for line in f:
      if 'color' in line:
-         new.write("<color>7f1b1c1d</color>\n")
+         new.write("<color>f31b1c1d</color>\n")
      else:
          new.write(line)
 
